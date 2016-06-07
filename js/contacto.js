f57824cba9c56042d58e818435bffb11f87409d9
unknown <sarita-coder>
2016-06-07 18:59:11 -0500

    js añadiendo

---
+++ js/contacto.js
@@ -0,0 +1,95 @@
+var contact=[{
+				id:"0",
+				name:"Laboratoria Perú",
+				img:"logocodeacademy.png",
+				chat:[],
+			},{
+				id:"1",
+				name:"Raymi Saldomando",
+				img:"raymi.jpg",
+				chat:[],
+			},{
+				id:"2",
+				name:"Mariana Costa",
+				img:"mariana.jpg",
+				chat:[],
+			},{
+				id:"3",
+				name:"Ana María Martinez Franklin",
+				img:"anamaria.jpg",
+				chat:[],
+			},{
+				id:"4",
+				name:"Rodulfo Prieto",
+				img:"rodulfo.jpg",
+				chat:[],
+			},{
+				id:"5",
+				name:"Andrea Lamas",
+				img:"andrea.jpg",
+				chat:[],
+			},{
+				id:"6",
+				name:"Maria Paula Rivarola",
+				img:"mariapaula.jpg",
+				chat:[],
+			},{
+				id:"7",
+				name:"Katy",
+				img:"katy.jpg",
+				chat:[],
+			},{
+				id:"8",
+				name:"aldo",
+				img:"aldo.jpg",
+				chat:[],
+			}];
+
+var gian=[{
+			name:"Laboratoria Perú",
+			id:"0",
+			chat:[],
+			hora:[],
+		},{
+			name:"Raymi Saldomando",
+			id:"1",
+			chat:[],
+			hora:[],
+		},{
+			name:"Mariana Costa",
+			id:"2",
+			chat:[],
+			hora:[],
+		},{
+			name:"Ana María Martinez Franklin",
+			id:"3",
+			chat:[],
+			hora:[],
+		},{
+			name:"Rodulfo Prieto",
+			id:"4",
+			chat:[],
+			hora:[],
+		},{
+			name:"Andrea Lamas",
+			id:"5",
+			chat:[],
+			hora:[],
+		},{
+			name:"Maria Paula Rivarola",
+			id:"6",
+			chat:[],
+			hora:[],
+		},{
+			name:"Katy Sanchez",
+			id:"7",
+			chat:[],
+			hora:[],
+		},{
+			name:"Aldo Alfaro",
+			id:"8",
+			chat:[],
+			hora:[],
+		}]
+//socket;
+//expres chat ;
