7ffa52706f2e48a27605c2e50886ff7a144e6a4f
sarita-coder <sarita.roman.colonio@gmail.com>
2016-06-08 14:49:31 -0500

    intentando socket

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-/**
- * Server.js
- * @author : DiganmeGiovanni | https://twitter.com/DiganmeGiovanni
- * @Created on: 25 Oct, 2014
- */
 
-/* Librerias necesarias para la aplicación */
-// var app  = require('express')();
-// var http = require('http').Server(app);
-var io   = require('socket.io').listen(3000);
-io.sockets.on("connection", arranque);
-function arranque (socket) {
-  socket.on("datos_cliente",regresar_datos);
-}
-function regresar_datos (data) {
-  io.sockets.emit("datos_servicor",data);  
-}
+// var io   = require('socket.io').listen(3000);
+// io.sockets.on("connection", arranque);
+// function arranque (socket) {
+//   socket.on("datos_cliente",regresar_datos);
+// }
+// function regresar_datos (data) {
+//   io.sockets.emit("datos_servicor",data);  
+// }
+var app  = require('express')();
+var http = require('http').Server(app);
+var io   = require('socket.io')(http);
 
 
 /** *** *** ***
@@ -22,45 +17,46 @@ function regresar_datos (data) {
  *  de manera que sin importar la ruta que el usuario solicite
  *  siempre lo direccionaremos al html del sistema de chat.
  */
-// app.get('*', function(req, res) {
-//   res.sendFile( __dirname + '/views/chat.html');
-// });
+app.get('*', function(req, res) {
+  res.sendFile( __dirname + '/chat.html');
+});
 
 
 /** *** *** ***
  *  Configuramos Socket.IO para estar a la escucha de
  *  nuevas conexiones.
-//  */
-// io.on('connection', function(socket) {
+ */
+io.on('connection', function(socket) {
   
-//   console.log('New user connected');
+  console.log('New user connected');
   
-//   /**
-//    * Cada nuevo socket debera estar a la escucha
-//    * del evento 'chat message', el cual se activa
-//    * cada vez que un usuario envia un mensaje.
-//    * 
-//    * @param  msg : Los datos enviados desde el cliente a 
-//    *               través del socket.
-//    */
-//   socket.on('chat message', function(msg) {
-//     io.emit('chat message', msg);
-//   });
+  /**
+   * Cada nuevo socket debera estar a la escucha
+   * del evento 'chat message', el cual se activa
+   * cada vez que un usuario envia un mensaje.
+   * 
+   * @param  msg : Los datos enviados desde el cliente a 
+   *               través del socket.
+   */
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
   
-//   /**
-//    * Mostramos en consola cada vez que un usuario
-//    * se desconecte del sistema.
-//    */
-//   socket.on('disconnect', function() {
-//     console.log('User disconnected');
-//   });
+  /**
+   * Mostramos en consola cada vez que un usuario
+   * se desconecte del sistema.
+   */
+  socket.on('disconnect', function() {
+    console.log('User disconnected');
+  });
   
-// });
+});
 
 
-// /**
-//  * Iniciamos la aplicación en el puerto 3000
-//  */
-// http.listen(3000, function() {
-//   console.log('listening on *:3000');
-// });
+/**
+ * Iniciamos la aplicación en el puerto 3000
+ */
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
+
